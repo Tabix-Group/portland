e3876fbe14a1b5c851658e2e9a1085a3977be6f0
hernanofx <hernandorra@gmail.com>
2025-07-17 16:23:35 -0300

    apis route

--- meeting-minute-manager/backend/src/app.js
+++ meeting-minute-manager/backend/src/app.js
@@ -9,12 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // Import routes
-app.use('/users', require('./routes/users'));
-app.use('/projects', require('./routes/projects'));
-app.use('/minutes', require('./routes/minutes'));
-app.use('/tasks', require('./routes/tasks'));
-app.use('/attachments', require('./routes/attachments'));
-app.use('/tags', require('./routes/tags'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/projects', require('./routes/projects'));
+app.use('/api/minutes', require('./routes/minutes'));
+app.use('/api/tasks', require('./routes/tasks'));
+app.use('/api/attachments', require('./routes/attachments'));
+app.use('/api/tags', require('./routes/tags'));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
