d25389459c5495aff4a07ec8a15e630bfefea80a
hernanofx <hernandorra@gmail.com>
2025-07-17 21:46:23 -0300

    intento fix 7

--- meeting-minute-manager/src/contexts/DataContext.tsx
+++ meeting-minute-manager/src/contexts/DataContext.tsx
@@ -364,12 +364,12 @@ export const DataProvider: React.FC<{ children: React.ReactNode }> = ({ children
   };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
-}
+};
 
 export const useData = () => {
-  const context = useContext(DataContext)
+  const context = useContext(DataContext);
   if (!context) {
-    throw new Error("useData must be used within a DataProvider")
+    throw new Error("useData must be used within a DataProvider");
   }
-  return context
-}
+  return context;
+};
